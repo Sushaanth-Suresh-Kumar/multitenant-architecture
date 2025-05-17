32eab964d739dc89a1e7e7f75b550b17cea37100
Sushaanth Suresh Kumar <sushaanthsureshkumartg@gmail.com>
2025-05-17 11:26:22 +0530

    fix: email service & code

--- src/main/java/dev/sushaanth/bookly/security/service/EmailService.java
+++ src/main/java/dev/sushaanth/bookly/security/service/EmailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
